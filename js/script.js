68ce0915e1be0a4a82736e0ffbe7dd9b726c9e64
LeandroCarlino <leandro.ca.98@gmail.com>
2022-10-24 12:59:02 -0300

    add button, css styles, and pdf

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-$(document).ready(function(){
-    $('.zoom').hover(function() {
-        $(this).addClass('transition');
-    }, function() {
-        $(this).removeClass('transition');
-    });
+$(document).ready(function () {
+  $(".zoom").hover(
+    function () {
+      $(this).addClass("transition");
+    },
+    function () {
+      $(this).removeClass("transition");
+    }
+  );
 });
+
+function openPDF() {
+  document.getElementById("pdf-container").style.display = "block";
+  document.getElementById("open-button").style.display = "none";
+  document.getElementById("close-button").style.display = "inline-block";
+}
+
+function closePDF() {
+  document.getElementById("pdf-container").style.display = "none";
+  document.getElementById("open-button").style.display = "inline-block";
+  document.getElementById("close-button").style.display = "none";
+}
