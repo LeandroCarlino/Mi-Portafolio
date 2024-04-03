627f5508f5486e66d68922af5b7d7027984d1780
Leandro C <leandro.ca.98@gmail.com>
2024-04-03 12:56:39 -0300

    actualize pdf and add age function

--- js/script.js
+++ js/script.js
@@ -21,4 +21,22 @@ function closePDF() {
   document.getElementById("close-button").style.display = "none";
 }
 
+function calculateAge(fechaNacimiento) {
+  let today = new Date();
+  let birthday = new Date(fechaNacimiento);
+  let age = today.getFullYear() - birthday.getFullYear();
+  let mes = today.getMonth() - birthday.getMonth();
+  if (mes < 0 || (mes === 0 && today.getDate() < birthday.getDate())) {
+      age--;
+  }
+  return age;
+}
+
+// Obtener la edad y actualizar el elemento HTML
+window.onload = function() {
+  let birthday = new Date('1998-07-06'); // Tu fecha de nacimiento
+  let age = calculateAge(birthday);
+  document.getElementById('age').textContent = age;
+};
+
 
